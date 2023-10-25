4926d39fa1c632da5e0b3cf617ac0a417d294136
Pitsco <derrickhuang2@gmail.com>
2023-10-24 21:12:03 -0700

    attempt at slide animation

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -31,3 +31,21 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll("[data-img-to-show]").forEach(section => {
   observer.observe(section)
 })
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
