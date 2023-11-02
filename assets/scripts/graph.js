a04a2fd1517cf964adbf685eae5c9cb2fc07b49d
YLu-1258 <maodou1258@gmail.com>
2023-11-02 03:53:58 -0700

    uodate

--- assets/scripts/graph.js
+++ assets/scripts/graph.js
@@ -305,14 +305,8 @@ function createLink(s, t) {
         const distance = link.attributes.distance;
         const maxId = Math.max(sId, tId);
 
-        let array = Array(maxId).fill().map(() => Array(maxId).fill(10000));
-
-        // while (adj_List.length < maxId) {
-        //     adj_List.push(Array(maxId).fill(10000));
-        // }
-
-        for (let i = 0; i < adj_List.length; i++) {
-            adj_List[i][i] = 0;
+        while (adj_List.length < maxId) {
+            adj_List.push(Array(maxId).fill(10000));
         }
 
         adj_List[sId - 1][tId - 1] = distance;
