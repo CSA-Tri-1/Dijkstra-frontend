81d0056831198d6cff535084b1d80067b81c0fbf
DavidVasilev1 <davidkvasilev@gmail.com>
2023-11-05 18:59:58 -0800

    final update

--- assets/scripts/graph.js
+++ assets/scripts/graph.js
@@ -1,6 +1,9 @@
+// initial variables for function and JointJS graph
 let startView;
 let endView;
+// path storage array
 let pathMembersViews = [];
+// JointJS vars for styling
 const pathMemberHighlightId = 'path-member';
 const invalidPathHighlightId = 'invalid-path-member';
 const pathMemberClassName = 'path-member';
@@ -17,8 +20,11 @@ const startAttrs = {
         'stroke-width': 2
     }
 };
+// initial node ID
 let nextId = 0;
+// control edit or view mode
 let editMode = true;
+// node styling
 const size = 40;
 const getTargetMarkerStyle = () => ({
     type: 'path',
@@ -29,6 +35,7 @@ const getTargetMarkerStyle = () => ({
         linkTools: []
     }
 });
+// link styling when highlighted
 const getLinkStyle = () => {
     return V.createSVGStyle(`
     .joint-link .${pathMemberClassName} {
@@ -36,9 +43,10 @@ const getLinkStyle = () => {
     }
 `);
 }
+// start and end views
 const getStartView = () => startView;
 const getEndView = () => endView;
-
+// setting up graph for JointJS
 const graph = new joint.dia.Graph;
 const paperElement = document.getElementById('interactive-graph');
 const paper = new joint.dia.Paper({
@@ -60,12 +68,13 @@ const paper = new joint.dia.Paper({
     linkPinning: false,
     async: true,
     frozen: false,
+    // makes graph interactive
     interactive: () => editMode,
+    // checks connections with links
     validateConnection: (cellViewS, _magnetS, cellViewT) => {
         const id = [cellViewS.model.id, cellViewT.model.id].sort().join();
         const existingLink = graph.getCell(id);
         const isSameCell = cellViewS.model.id === cellViewT.model.id;
-
         return !isSameCell && !existingLink && !cellViewT.model.isLink();
     },
     highlighting: {
@@ -83,6 +92,7 @@ const paper = new joint.dia.Paper({
 });
 var namespace = joint.shapes;
 
+// reloads page and clears graph to restart for user
 document.getElementById('restart-button').addEventListener('click', function(evt) {
     editMode = evt.target.checked
     location.reload();
@@ -90,17 +100,20 @@ document.getElementById('restart-button').addEventListener('click', function(evt
     graph.clear();
 });
 
+// listener controller for Joint graph
 class Controller extends joint.mvc.Listener {
     get context() {
         const [ctx = null] = this.callbackArguments;
         return ctx;
     }
-  }
+}
 
-  class ViewController extends Controller {
+// view controller, controlling display of shortest path with listeners
+class ViewController extends Controller {
     startListening() {
         const { paper } = this.context;
 
+        // activate functions with certain listeners
         this.listenTo(paper, {
             'element:pointerdown': selectSource,
             'element:mouseenter': selectEnd,
@@ -109,67 +122,79 @@ class Controller extends joint.mvc.Listener {
     }
 }
 
+// selecting the start node
 function selectSource({ setStartView }, elementView) {
     setStartView(elementView);
 }
 
+// hovering over the end node (displays entire path)
 function selectEnd({ showPath, setEndView, getStartView, getEndView }, elementView) {
     const pathStartView = getStartView();
     const pathEndView = getEndView();
 
+    // checks for if start and end are the same
     if (elementView === pathStartView) return;
+
+    // checks for invalid path connection
     if (pathStartView && pathEndView) {
         joint.highlighters.addClass.remove(pathStartView, invalidPathHighlightId);
         joint.highlighters.addClass.remove(pathEndView, invalidPathHighlightId);
     }
     setEndView(elementView);
+
+    // shows path
     showPath();
 }
 
+// hide path and highlight on mouse leave
 function hidePathOnMouseLeave({ hidePath, getStartView, getEndView, setEndView }) {
     const pathStartView = getStartView();
     const pathEndView = getEndView();
-
+    
+    // hides path
     hidePath();
+
+    // check for invalid path
     if (pathStartView) joint.highlighters.addClass.remove(pathStartView, invalidPathHighlightId);
     if (pathEndView) joint.highlighters.addClass.remove(pathEndView, invalidPathHighlightId);
+
+    // reset end node
     setEndView(null);
 }
 
-
+// edit controller with listening
 class EditController extends Controller {
     startListening() {
         const { graph, paper } = this.context;
 
+        // adding links
         this.listenTo(graph, {
             'change:source': replaceLink,
             'change:target': replaceLink,
         });
 
+        // adding nodes
         this.listenTo(paper, {
             'element:mouseenter': showElementTools,
             'element:mouseleave': hideElementTools,
-            'element:pointerdblclick': removeElement,
             'blank:pointerdblclick': addElement
         });
     }
 }
 
+// elements tools, allow for editing
 function showElementTools(_context, elementView, _evt) {
     elementView.showTools();
 }
-
 function hideElementTools(_context, elementView) {
     elementView.hideTools();
 }
 
-// When a new link is created via UI (in Edit mode), remove the previous link
-// and create a new one that has the ID constructed as "nodeA,nodeB". The
-// reason we're removing the old link below is that it is not a good idea
-// to change ID's of any model in JointJS.
+// adding link function
 function replaceLink({ createLink }, link, _collection, opt) {
     const sourceId = link.get('source').id;
     const targetId = link.get('target').id;
+    // this is done to prevent JointJS from breaking
     if (opt.ui && sourceId && targetId) {
         createLink(sourceId, targetId);
         link.remove();
@@ -177,22 +202,15 @@ function replaceLink({ createLink }, link, _collection, opt) {
     
 }
 
-function removeElement({ setStartView, setEndView, getStartView }, elementView) {
-    const pathStart = getStartView();
-    if (elementView.model.id === pathStart.model.id) {
-        setStartView(null);
-        setEndView(null);
-    }
-    
-    elementView.model.remove();
-}
-
+// adding node and logging it for backend in array
 function addElement({ createNode, size }, _evt, x, y) {
+    // node function creating node
     const node = createNode(getNodeId(), x - size / 2, y - size / 2);
     node.position(x - size / 2, y - size / 2);
     nodes_array.push(node);
 }
 
+// listens for change in node position to update for calculating weight
 graph.on('change:position', function(cell) {
     if (cell.isElement()) {
         const nodeId = cell.id;
@@ -201,19 +219,25 @@ graph.on('change:position', function(cell) {
     }
 });
 
+// init controller subclasses
 const viewController = new ViewController({ paper, showPath, hidePath, setStartView, setEndView, getStartView, getEndView });
 const editController = new EditController({ graph, paper, createLink, createNode, setStartView, setEndView, getStartView, size });
 
+// begins listening for edits
 editController.startListening();
 
+// finds current index of node in question
 function getCurrentID() {
     return current_index;
 }
+
+// used to get next nodeID
 function getNodeId() {
     current_index++;
     return current_index;
 }
 
+// get node by specific ID
 function getNodefromId(id) {
     for (let i = 0; i < nodes_array.length; i++) {
         if (nodes_array[i].id == id) {
@@ -222,7 +246,9 @@ function getNodefromId(id) {
     }
 }
 
+// creating node
 function createNode(id) {
+    // set up node for JointJS with attributes
     var node = new joint.shapes.standard.Circle({
         id,
         size: { width: 40, height: 40 },
@@ -236,6 +262,8 @@ function createNode(id) {
         }
     }).addTo(graph);
     var view = node.findView(paper);
+
+    // sets up tools that are used for node connection with links
     view.addTools(new joint.dia.ToolsView({
         tools: [
             new joint.elementTools.HoverConnect({
@@ -245,6 +273,7 @@ function createNode(id) {
         ]
     }))
     
+    // sets view and tools in use
     view.hideTools();
     node.attr('label/text', id);
     return node;
@@ -252,10 +281,12 @@ function createNode(id) {
 
 // creating links between nodes on map
 function createLink(s, t) {
+    // init for distance formula
     let x1 = getNodefromId(s).attributes.position.x
     let x2 = getNodefromId(t).attributes.position.x
     let y1 = getNodefromId(s).attributes.position.y
     let y2 = getNodefromId(t).attributes.position.y
+    // default settings for link styling and attributes
     var link = new joint.shapes.standard.Link({
         id: [s, t].sort().join(),
         source: { id: s },
@@ -277,6 +308,7 @@ function createLink(s, t) {
         },
     });
 
+    // visual representation of links
     link.appendLabel({
         attrs: {
             text: {
@@ -292,6 +324,7 @@ function createLink(s, t) {
         }
     });
 
+    // creation of adjacency list for display of path
     if (link.attributes.target.hasOwnProperty("id")) {
 
         // push edge weights for adj_array
@@ -299,18 +332,13 @@ function createLink(s, t) {
         const tId = link.attributes.target.id;
         const distance = link.attributes.distance;
 
-
-        const maxId = Math.max(sId, tId);
-
-        while (adj_List.length < maxId) {
-            adj_List.push(Array(maxId).fill(10000));
-        }
-
+        // fills in second adjacency array to later build main adjacency list
         adj_array.push([sId - 1, tId - 1, distance])
     }
 
     link.addTo(graph);
     
+    // display of links, styling
     var view = link.findView(paper);
     view.addTools(new joint.dia.ToolsView({
         tools: []
@@ -320,45 +348,61 @@ function createLink(s, t) {
 }
 
 
-
+// setting start view of graph
 function setStartView(elementView) {
+    // hide path if visible
     hidePath();
+
+    // removes old start node when clicked
     if (startView) {
         joint.highlighters.mask.remove(startView, highlightId);
         joint.highlighters.addClass.remove(startView, invalidPathHighlightId);
     }
 
+    // if the start is an end node, remove end node
     if (endView) {
         joint.highlighters.addClass.remove(endView, invalidPathHighlightId);
     }
 
+    // change styling on new start node
     if (elementView) {
         joint.highlighters.mask.add(elementView, 'body', highlightId, startAttrs);
     }
+    // define new start
     startView = elementView;
+    // send data to backend
     start.length = 0
     start.push(elementView.model.id)
 }
 
+// setting end node
 function setEndView(elementView) {
+    // function activated when hover
     endView = elementView
+    // send data to backend
     end.length = 0
     end.push(elementView.model.id)
 }
 
+// get path from backend
 function getElementPath() {
+    // debug
     console.log('Current start and end values:', start, end);
     console.log('Current adj_List:', adj_List);
 
+    // create adjacency list by filling all with infinity
     adj_List = Array(current_index).fill().map(() => Array(current_index).fill(10000));
+    // using weight data from second adj list for main adj list
     for (let i = 0; i < adj_array.length; i++) {
       adj_List[adj_array[i][0]][adj_array[i][1]] = adj_array[i][2];
       adj_List[adj_array[i][1]][adj_array[i][0]] = adj_array[i][2];
     }
+    // setting empty values with 0 for nodes connected to themselves
     for (let i = 0; i < current_index; i ++){
       adj_List[i][i] = 0;
     }
 
+    // defines data set to backend
     const payload = {
         adjacencyList: adj_List,
         source: parseInt(start),
@@ -374,7 +418,8 @@ function getElementPath() {
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     // Handling the response from the server
-    return new Promise((resolve, reject) => {
+    // uses promises to wait for response before displaying
+    return new Promise((resolve) => {
         xhr.onload = function() {
         
             if(xhr.status >= 200 && xhr.status < 300) {
@@ -397,16 +442,19 @@ function getElementPath() {
     });
 }
 
+// getting path from backend
 function getLinkPath(elementPath) {
     const linkPath = [];
 
+    // finds all paths connecting nodes
     if (startView) {
         for (let i = 0; i < elementPath.length - 1; i++) {
             const sourceId = elementPath[i];
             const targetId = elementPath[i + 1];
             const link = graph.getCell([sourceId, targetId].sort().join());
+            // checks for if element is link
             if (!link) continue;
-
+            // sends the links to showPath()
             linkPath.push(link.id);
         }
     }
@@ -414,46 +462,55 @@ function getLinkPath(elementPath) {
     return linkPath;
 }
 
+// showing path
 async function showPath() {
-   
-        const elementPath = await getElementPath();
-        const isPathFound = elementPath.length > 0;
-        if (!isPathFound && startView && endView && startView.id !== endView.id && !editMode) {
-            joint.highlighters.addClass.add(startView, 'body', invalidPathHighlightId, {
-                className: invalidPathClassName
-            });
-            joint.highlighters.addClass.add(endView, 'body', invalidPathHighlightId, {
-                className: invalidPathClassName
-            });
-            hidePath();
-            return;
-        }
-    
-        if (startView) joint.highlighters.addClass.remove(startView, invalidPathHighlightId);
-        if (endView) joint.highlighters.addClass.remove(endView, invalidPathHighlightId);
-        hidePath();
-        const linkPath = getLinkPath(elementPath);
-    
-        for (const elementId of [...elementPath, ...linkPath]) {
-            const element = graph.getCell(elementId);
-            const view = element.findView(paper);
-            const isLink = view.model.isLink();
-            joint.highlighters.addClass.add(view, isLink ? 'line' : 'body', pathMemberHighlightId, {
-                className: pathMemberClassName
-            });
-    
-            if (isLink) {
-                element.set('z', 2);
-            }
-    
-            pathMembersViews.push(view);
-        }
-   
 
-    // document.getElementById('path').innerText = elementPath.join(' → ');
+    // sets element path from data from backend, awaiting for response
+    const elementPath = await getElementPath();
+    // checks if path is found
+    const isPathFound = elementPath.length > 0;
+    // highlights for non-existent path
+    if (!isPathFound && startView && endView && startView.id !== endView.id && !editMode) {
+        joint.highlighters.addClass.add(startView, 'body', invalidPathHighlightId, {
+            className: invalidPathClassName
+        });
+        joint.highlighters.addClass.add(endView, 'body', invalidPathHighlightId, {
+            className: invalidPathClassName
+        });
+        // doesn't show path
+        hidePath();
+        return;
+    }
+
+    // removes if start and end don't exist
+    if (startView) joint.highlighters.addClass.remove(startView, invalidPathHighlightId);
+    if (endView) joint.highlighters.addClass.remove(endView, invalidPathHighlightId);
+    hidePath();
+
+    // gets link path between node path
+    const linkPath = getLinkPath(elementPath);
+    // display all elements
+    for (const elementId of [...elementPath, ...linkPath]) {
+        const element = graph.getCell(elementId);
+        const view = element.findView(paper);
+        const isLink = view.model.isLink();
+        // styles nodes
+        joint.highlighters.addClass.add(view, isLink ? 'line' : 'body', pathMemberHighlightId, {
+            className: pathMemberClassName
+        });
+        // sets link styles
+        if (isLink) {
+            element.set('z', 2);
+        }
+
+        // creates path members array
+        pathMembersViews.push(view);
+    }
 }
 
+// hiding path
 function hidePath() {
+    // reset styling for all elements on path array
     for (const view of pathMembersViews) {
         const model = view.model;
         joint.highlighters.addClass.remove(view, pathMemberHighlightId);
@@ -464,16 +521,21 @@ function hidePath() {
         }
     }
 
+    // clear array
     pathMembersViews = [];
 }
 
+// creating link style
 function toggleLinkStyle() {
+    // check for existing style and remove
     if (linkStyle) paper.svg.removeChild(linkStyle);
 
+    // setting canvas styles
     linkStyle = getLinkStyle();
     paper.svg.prepend(linkStyle);
 }
 
+// styles of the graph members
 const styles = V.createSVGStyle(`
     .joint-element .${pathMemberClassName} {
         stroke: ${blueColor};
@@ -497,36 +559,49 @@ const styles = V.createSVGStyle(`
     }
 `);
 
+// setting styles
 let linkStyle = getLinkStyle();
 
+// creating styles
 paper.svg.prepend(styles);
 paper.svg.prepend(linkStyle);
 
+// zoom
 var zoomLevel = 1;
 
+// zooming in
 document.getElementById('zoom-in').addEventListener('click', function() {
+    // enlarging all elements on canvas
     zoomLevel = Math.min(3, zoomLevel + 0.2);
     var size = paper.getComputedSize();
     paper.translate(0,0);
     paper.scale(zoomLevel, zoomLevel, size.width / 2, size.height / 2);
 });
 
+// zooming out
 document.getElementById('zoom-out').addEventListener('click', function() {
+    // minimizing all elements on canvas
     zoomLevel = Math.max(0.2, zoomLevel - 0.2);
     var size = paper.getComputedSize();
     paper.translate(0,0);
     paper.scale(zoomLevel, zoomLevel, size.width / 2, size.height / 2);
 });
 
+// switch between view and edit controllers
 function toggleView() {
-    for(const element of graph.getElements()) {
+    // change cursor style
+    for (const element of graph.getElements()) {
         element.attr('body/cursor', editMode ? 'move' : 'pointer');
     }
 
+    // start edit mode listening
     if (editMode) {
+        //listening functions
         viewController.stopListening();
         editController.startListening();
+        // hide paths
         hidePath();
+        // removing styling of start and end
         if (startView) {
             joint.highlighters.mask.remove(startView, highlightId);
             joint.highlighters.addClass.remove(startView, invalidPathHighlightId);
@@ -534,16 +609,21 @@ function toggleView() {
         if (endView) {
             joint.highlighters.addClass.remove(endView, invalidPathHighlightId);
         }
+    // starting view mode listening
     } else {
+        // turn off editing listening
         viewController.startListening();
         editController.stopListening();
+        // show path
         showPath();
+        // create styling for start
         if (startView) {
             joint.highlighters.mask.add(startView, 'body', highlightId, startAttrs);
         }
     }
 }
 
+// toggle from edit to view and back
 document.getElementById('edit-mode-toggle').addEventListener('click', function(evt) {
     editMode = evt.target.checked
     toggleView();
